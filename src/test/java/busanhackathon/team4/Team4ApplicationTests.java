c714440a441f1a0ea85205974170af6eb7b012ee
dgjinsu <rlawlstn3206@naver.com>
2023-07-12 16:21:13 +0900

    first commit

---
+++ src/test/java/busanhackathon/team4/Team4ApplicationTests.java
@@ -0,0 +1,13 @@
+package busanhackathon.team4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
