57214b750b82642647f6fbbc19ac843e4158d30b
dgjinsu <rlawlstn3206@naver.com>
2023-07-13 13:52:32 +0900

    fixed: repo 파라미터 바인딩 에러 해결

--- src/main/java/busanhackathon/team4/recipe/repository/RecipeRepository.java
+++ src/main/java/busanhackathon/team4/recipe/repository/RecipeRepository.java
@@ -3,12 +3,13 @@ package busanhackathon.team4.recipe.repository;
 import busanhackathon.team4.recipe.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    @Query("select r from Recipe r where r.member.id =: us")
-    List<Recipe> findAllRecipeByMember(Long username);
+    @Query("select r from Recipe r where r.member.id = :username")
+    List<Recipe> findAllRecipeByMember(@Param("memberId") Long username);
 }
